9930a89ab591fcf09118cc45f38525a3861edaab
Xavier Terés <66311413+xavierteres@users.noreply.github.com>
2022-12-28 16:38:31 +0100

    Add db uploads methods

--- utils/db.py
+++ utils/db.py
@@ -4,10 +4,10 @@ import psycopg2
 
 def get_db_connection():
     conn = psycopg2.connect(
-        host="db",
+        host="localhost",
         database="flask_db",
-        user=os.environ["DB_USERNAME"],
-        password=os.environ["DB_PASSWORD"],
+        user="postgres",
+        password="postgres",
     )
     return conn
 
@@ -75,3 +75,39 @@ def insert_user(username, paid):
     conn.close()
 
     return user_inserted
+
+def insert_upload(sid, removeKey):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO uploads(sid, remove) VALUES ('{}', '{}') RETURNING *;".format(
+            sid, removeKey
+        )
+    )
+    conn.commit()
+    upload_inserted = cur.fetchone
+    cur.close()
+    conn.close()
+
+    return upload_inserted
+
+def get_upload(sid):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM uploads WHERE sid = '{}';".format(sid))
+    upload = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    return upload
+
+def remove_upload(sid):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM uploads WHERE sid = '{}';".format(sid))
+    conn.commit()
+    uploads_deleted = cur.rowcount
+    cur.close()
+    conn.close()
+
+    return uploads_deleted
